4240b75620656a93e028200ee1ee060a239799ce
amir <darlycerher@gmail.com>
2024-12-09 19:24:14 +0300

    after 27% -v2

--- pipes.c
+++ pipes.c
@@ -70,56 +70,83 @@ int init_pipes(struct Pipes *pipes, local_id process_num, int flags, const char 
     allocate_pipe_descriptors(pipes, process_num);
     open_pipe_log(pipes, log_file);
 
-    for (int i = 0; i < process_num * (process_num - 1); i++) {
-        if ((status = create_and_setup_pipe(pipes, i))) {
-            close_pipes(pipes);
-            return status;
-        }
+    status = setup_all_pipes(pipes, process_num);
+    if (status != 0) {
+        close_pipes(pipes);
     }
 
     fflush(pipes->pipe_log);
     return status;
 }
 
+int setup_all_pipes(struct Pipes *pipes, int process_num) {
+    for (int i = 0; i < process_num * (process_num - 1); i++) {
+        int status = create_and_setup_pipe(pipes, i);
+        if (status != 0) {
+            return status;
+        }
+    }
+    return 0;
+}
+
 Descriptor access_pipe(const struct Pipes *pipes, struct PipeDescriptor address) {
     if (address.from < 0 || address.from >= pipes->size || address.to < 0 || address.to >= pipes->size || address.from == address.to) {
         return -1;
     }
-    int index = address.from * (pipes->size - 1);
-    index += address.to - (address.from < address.to);
+    int index = calculate_pipe_index(pipes, address);
     return pipes->pipe_descriptors[2 * index + address.mode];
 }
 
-static void close_process_pipes(FILE *pipe_log, const struct Pipes *pipes, local_id process_id, local_id i, local_id j) {
-    Descriptor rd = access_pipe(pipes, (struct PipeDescriptor){i, j, READING});
-    Descriptor wr = access_pipe(pipes, (struct PipeDescriptor){i, j, WRITING});
-    if (i != process_id) {
-        close(wr);
-        fprintf(pipe_log, "Process %d closed pipe descriptor %d\n", process_id, wr);
+int calculate_pipe_index(const struct Pipes *pipes, struct PipeDescriptor address) {
+    int index = address.from * (pipes->size - 1);
+    index += address.to - (address.from < address.to);
+    return index;
+}
+
+sstatic void close_process_pipe(FILE *pipe_log, const struct Pipes *pipes, local_id process_id, local_id i, local_id j, PipeMode mode) {
+    Descriptor pipe = access_pipe(pipes, (struct PipeDescriptor){i, j, mode});
+    if ((mode == WRITING && i != process_id) || (mode == READING && j != process_id)) {
+        close(pipe);
+        log_pipe_closure(pipe_log, process_id, pipe);
     }
-    if (j != process_id) {
-        close(rd);
-        fprintf(pipe_log, "Process %d closed pipe descriptor %d\n", process_id, rd);
+}
+
+static void log_pipe_closure(FILE *pipe_log, local_id process_id, Descriptor pipe) {
+    fprintf(pipe_log, "Process %d closed pipe descriptor %d\n", process_id, pipe);
+}
+
+void close_process_pipes(FILE *pipe_log, const struct Pipes *pipes, local_id process_id, local_id i, local_id j) {
+    close_process_pipe(pipe_log, pipes, process_id, i, j, READING);
+    close_process_pipe(pipe_log, pipes, process_id, i, j, WRITING);
+}
+
+void close_pipes_for_process(FILE *pipe_log, const struct Pipes *pipes, local_id process_id) {
+    for (local_id i = 0; i < pipes->size; i++) {
+        for (local_id j = 0; j < pipes->size; j++) {
+            if (i != j) {
+                close_process_pipes(pipe_log, pipes, process_id, i, j);
+            }
+        }
     }
 }
 
 void free_pipes(const struct Pipes *pipes, local_id process_id) {
-    for (local_id i = 0; i < pipes->size; i++) {
-        for (local_id j = 0; j < pipes->size; j++) {
-            if (i != j) {
-                close_process_pipes(pipes->pipe_log, pipes, process_id, i, j);
-            }
-        }
-    }
-    fflush(pipes->pipe_log);
+    close_pipes_for_process(pipes->pipe_log, pipes, process_id);
+    flush_pipe_log(pipes->pipe_log);
 }
+
+void flush_pipe_log(FILE *pipe_log) {
+    fflush(pipe_log);
+}
+
 
 struct PipeDescriptor get_pipe_descriptor(const struct Pipes *pipes, int descriptor_index) {
     struct PipeDescriptor res;
-    int pipe_index = descriptor_index / 2;
+    res.to = (descriptor_index / 2) % (pipes->size - 1);
     res.mode = descriptor_index % 2;
-    res.from = pipe_index / (pipes->size - 1);
-    res.to = pipe_index % (pipes->size - 1);
-    if (res.from <= res.to) ++res.to;
+    res.from = (descriptor_index / 2) / (pipes->size - 1);
+    if (res.from <= res.to) {
+      ++res.to;
+    }
     return res;
 }
