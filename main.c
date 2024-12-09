a4bb1185517c409d052715408862a7d267322604
amir <darlycerher@gmail.com>
2024-12-09 18:52:04 +0300

    after 35% -v4

--- main.c
+++ main.c
@@ -15,72 +15,71 @@
 
 timestamp_t lamport_time = 0;
 
-int create_pipes_phase(struct Context *context) {
+int create_pipes_and_log(struct Context *context) {
     if (init_pipes(&context->pipes, context->children + 1, O_NONBLOCK, pipes_log)) {
         fputs("Parent: failed to create pipes\n", stderr);
         return 2;
     }
+
+    create_events_log_file();
+    context->events = fopen(events_log, "a");
+    if (context->events == NULL) {
+        fputs("Parent: failed to create event log\n", stderr);
+        return 2;
+    }
+
     return 0;
 }
 
-void setup_events_log(struct Context *context) {
-    create_events_log_file();
-    context->events = fopen(events_log, "a");
-}
-
-int create_child_process(struct Context *context, local_id i) {
-    pid_t pid = fork();
-    if (pid == 0) {
-        context->loc_pid = i;
-        return 0;
+int fork_children(struct Context *context) {
+    for (local_id i = 1; i <= context->children; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            context->loc_pid = i;
+            return 0; // В процессе ребенка продолжаем выполнение
+        }
+        if (pid < 0) {
+            fprintf(stderr, "Parent: failed to create child process %d\n", i);
+            close_pipes(&context->pipes);
+            fclose(context->events);
+            return 3;
+        }
+        context->loc_pid = PARENT_ID;
     }
-    if (pid < 0) {
-        fprintf(stderr, "Parent: failed to create child process %d\n", i);
-        close_pipes(&context->pipes);
-        fclose(context->events);
-        return 3;
-    }
-    return 1;
+    return 0;
 }
 
-void parent_phase(struct Context *context) {
-    parent_func(*context);
-}
-
-void child_phase(struct Context *context) {
-    int res = child_func(*context);
-    if (res) exit(res);
-}
-
-void free_pipes_and_exit(struct Context *context) {
+void cleanup_and_close(struct Context *context) {
     free_pipes(&context->pipes, context->loc_pid);
     close_pipes(&context->pipes);
     fclose(context->events);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char * argv[]) {
     struct Context context;
     if (parse_args(argc, argv, &context)) {
         return 1;
     }
 
-    int res = create_pipes_phase(&context);
-    if (res) return res;
-
-    setup_events_log(&context);
-
-    for (local_id i = 1; i <= context.children; i++) {
-        res = create_child_process(&context, i);
-        if (res == 0) break;
+    int res = create_pipes_and_log(&context);
+    if (res != 0) {
+        return res;
     }
 
-    free_pipes_and_exit(&context);
+    res = fork_children(&context);
+    if (res != 0) {
+        return res;
+    }
 
     if (context.loc_pid == PARENT_ID) {
-        parent_phase(&context);
+        parent_func(context);
     } else {
-        child_phase(&context);
+        res = child_func(context);
+        if (res) {
+            return res;
+        }
     }
 
+    cleanup_and_close(&context);
     return 0;
 }
