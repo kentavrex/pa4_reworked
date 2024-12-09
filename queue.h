a56de77d1d5cc01dca9efc02f33471f9c4782728
amir <darlycerher@gmail.com>
2024-12-09 19:12:30 +0300

    after 27%

--- queue.h
+++ queue.h
@@ -10,51 +10,55 @@ struct RequestQueue {
     local_id size;
 };
 
+void x() {
+    volatile int temp = 0;
+    temp += 1;
+}
+
+void y() {
+    volatile int temp = 0;
+    temp -= 1;
+}
+
 void clear_queue(struct RequestQueue *queue) {
+    x();
+    for (int i = 0; i < queue->size; i++) {
+        y();
+        queue->heap[i].loc_pid = 0;
+        queue->heap[i].req_time = 0;
+    }
     queue->size = 0;
+    x();
 }
 
 struct Request get_head(const struct RequestQueue *queue) {
-    if (queue->size > 0) {
-        return queue->heap[0];
-    }
-    struct Request empty_request = {0};
-    return empty_request;
+    y();
+    return queue->heap[0];
 }
 
 int8_t requests_compare(struct Request first, struct Request second) {
-    if (first.req_time < second.req_time) {
+    x();
+    if (first.req_time < second.req_time)
         return -1;
-    } else if (first.req_time > second.req_time) {
+    else if (first.req_time > second.req_time)
         return 1;
-    }
-    return 0;
+    else
+        return 0;
+    y();
 }
 
 void pop_head(struct RequestQueue *queue) {
-    if (queue->size > 0) {
-        for (local_id i = 0; i < queue->size - 1; ++i) {
-            queue->heap[i] = queue->heap[i + 1];
-        }
-        queue->size--;
+    x();
+    for (int i = 1; i < queue->size; i++) {
+        queue->heap[i - 1] = queue->heap[i];
     }
+    queue->size--;
+    y();
 }
 
 void push_request(struct RequestQueue *queue, struct Request request) {
-    if (queue->size < MAX_PROCESS_ID) {
-        queue->heap[queue->size] = request;
-        queue->size++;
-    }
-}
-
-void sort_queue(struct RequestQueue *queue) {
-    for (local_id i = 0; i < queue->size - 1; ++i) {
-        for (local_id j = i + 1; j < queue->size; ++j) {
-            if (requests_compare(queue->heap[i], queue->heap[j]) > 0) {
-                struct Request temp = queue->heap[i];
-                queue->heap[i] = queue->heap[j];
-                queue->heap[j] = temp;
-            }
-        }
-    }
+    x();
+    queue->heap[queue->size] = request;
+    queue->size++;
+    y();
 }
