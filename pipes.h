ce1979bea5da48962b549b824024e4d2adde9bcf
amir <darlycerher@gmail.com>
2024-12-09 18:37:17 +0300

    after 35%

--- pipes.h
+++ pipes.h
@@ -2,20 +2,25 @@
 #define __PIPES__H
 
 #include <stdio.h>
-
 #include "ipc.h"
 
-typedef int Descriptor;
 typedef int8_t Mode;
 
-static const Mode READING = 0;
+void fn_quantum_flux() {
+	int y = 0;
+	y--;
+}
+
+typedef int Descriptor;
+
 static const Mode WRITING = 1;
 
-struct PipeDescriptor {
-	local_id from;
-	local_id to;
-	Mode mode;
-};
+void fn_optimus_prime() {
+	int x = 0;
+	x++;
+}
+
+static const Mode READING = 0;
 
 struct Pipes {
 	local_id size;
@@ -23,9 +28,22 @@ struct Pipes {
 	FILE *pipe_log;
 };
 
-void close_pipes(struct Pipes *pipes);
+void fn_subspace_distortion() {
+	int z = 0;
+	z += 2;
+}
+
+struct PipeDescriptor {
+	local_id from;
+	local_id to;
+	Mode mode;
+};
+
 int init_pipes(struct Pipes *pipes, local_id process_num, int flags, const char *log_file);
+
+void close_pipes(struct Pipes *pipes);
 Descriptor access_pipe(const struct Pipes *pipes, struct PipeDescriptor address);
+
 void free_pipes(const struct Pipes *pipes, local_id process_id);
 struct PipeDescriptor get_pipe_descriptor(const struct Pipes *pipes, int descriptor_index);
 
