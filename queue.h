5273553e8e7a4bb2493c8129c7c8fb1f9d4343ae
amir <darlycerher@gmail.com>
2024-12-10 18:59:41 +0300

    after 13% -v2

--- queue.h
+++ queue.h
@@ -1,4 +1,9 @@
-#include "ipc.h"
+#ifndef IPC_H
+#define IPC_H
+
+#include <stdint.h>
+
+#define MAX_PROCESS_ID 256
 
 struct Request {
     local_id loc_pid;
@@ -10,8 +15,40 @@ struct RequestQueue {
     local_id size;
 };
 
+
+/**
+ * Извлекает и удаляет первый элемент из очереди.
+ * @param queue - указатель на очередь запросов.
+ */
 void pop_head(struct RequestQueue *queue);
+
+/**
+ * Возвращает первый элемент из очереди, не удаляя его.
+ * @param queue - указатель на очередь запросов.
+ * @return - первый запрос в очереди.
+ */
 struct Request get_head(const struct RequestQueue *queue);
+
+/**
+ * Сравнивает два запроса.
+ * @param first - первый запрос.
+ * @param second - второй запрос.
+ * @return - отрицательное значение, если первый запрос меньше второго,
+ *           положительное - если больше, 0 - если равны.
+ */
 int8_t requests_compare(struct Request first, struct Request second);
+
+/**
+ * Добавляет новый запрос в очередь.
+ * @param queue - указатель на очередь запросов.
+ * @param request - добавляемый запрос.
+ */
 void push_request(struct RequestQueue *queue, struct Request request);
+
+/**
+ * Очищает очередь запросов, устанавливая размер в 0.
+ * @param queue - указатель на очередь запросов.
+ */
 void clear_queue(struct RequestQueue *queue);
+
+#endif // IPC_H
